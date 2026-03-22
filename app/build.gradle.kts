a6c866e1651cc3463bfa2258bd8c8d7b98aacdca
re-ovo <1609403959@qq.com>
2026-03-22 16:04:18 +0800

    chore: bump to 2.1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "me.rerere.rikkahub"
         minSdk = 26
         targetSdk = 36
-        versionCode = 148
-        versionName = "2.1.5"
+        versionCode = 149
+        versionName = "2.1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
